d2379bf173d459aa14ceedc186515c55e7ac4f26
Aaron Calzado <heythere@aaroncalzado.com>
2014-03-11 17:36:56 -0700

    Added Intro and Ending section to Project pages.

--- site/templates/project.php
+++ site/templates/project.php
@@ -3,9 +3,11 @@
 		<section class="content">
 			<div class="containerProject">
 				<div class="copyBody">
+					<?php echo kirbytext($page->intro()) ?>
 					<?php echo kirbytext($page->text()) ?>
 					<?php echo kirbytext($page->breakdown()) ?>
 					<?php echo kirbytext($page->guts()) ?>
+					<?php echo kirbytext($page->ending()) ?>
 				</div>
 			</div>
 			<?php snippet('footer') ?>
