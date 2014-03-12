3b72a9792ae9dc3b4e60fae8c610de714b8e5800
Aaron Calzado <heythere@aaroncalzado.com>
2014-03-11 17:11:16 -0700

    Added new sections Breakdown and Guts for Projects page. Renamed an image to heroImage.

--- site/templates/project.php
+++ site/templates/project.php
@@ -3,7 +3,9 @@
 		<section class="content">
 			<div class="containerProject">
 				<div class="copyBody">
-					<?php echo kirbytext($page->text()) ?>			
+					<?php echo kirbytext($page->text()) ?>
+					<?php echo kirbytext($page->breakdown()) ?>
+					<?php echo kirbytext($page->guts()) ?>
 				</div>
 			</div>
 			<?php snippet('footer') ?>
