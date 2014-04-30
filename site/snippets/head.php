acdcff448781621944e46c4b076ebe38f8df3bfc
Aaron Calzado <heythere@aaroncalzado.com>
2014-04-29 18:14:54 -0700

    Added Open Web Analytics script to head.php.

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -18,6 +18,26 @@
     });
   </script>
 
+  <!-- Start Open Web Analytics Tracker -->
+  <script type="text/javascript">
+  //<![CDATA[
+  var owa_baseUrl = 'http://aaroncalzado.com/owa/';
+  var owa_cmds = owa_cmds || [];
+  owa_cmds.push(['setSiteId', '5957baa3da25af0c4d4d287698069c82']);
+  owa_cmds.push(['trackPageView']);
+  owa_cmds.push(['trackClicks']);
+  owa_cmds.push(['trackDomStream']);
+
+  (function() {
+    var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+    owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+    _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+    var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+  }());
+  //]]>
+  </script>
+  <!-- End Open Web Analytics Code -->
+
   <title>
     <?php echo html($site->title()) ?> - <?php echo html($page->title()) ?>
   </title>
@@ -28,6 +48,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <?php echo css('assets/styles/styles.css') ?>
   <?php echo css('assets/styles/animate.css') ?>
-  <?php echo css('assets/styles/skeleton.css') ?>
-  <?php echo css('assets/styles/layout.css') ?>
 </head>
